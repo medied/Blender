c67f05090a06151e550b8d6ef266488c6dfd9cce
Eduardo <medina.eag@gmail.com>
2020-07-19 20:41:27 -0400

    Add SpecialBlender.php

---
+++ includes/SpecialBlender.php
@@ -0,0 +1,20 @@
+<?php
+class SpecialBlender extends SpecialPage {
+	function __construct() {
+		parent::__construct( 'Blender' );
+	}
+
+	function execute( $par ) {
+		$request = $this->getRequest();
+		$output = $this->getOutput();
+		$this->setHeaders();
+
+		# Get request data from, e.g.
+		# $param = $request->getText( 'param' );
+
+		# Do stuff
+		# ...
+		$wikitext = 'Hello world!';
+		$output->addWikiTextAsInterface( $wikitext );
+	}
+}
