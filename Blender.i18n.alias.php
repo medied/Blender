a0281734a65d40ce18b4f9d37611d061b7a5b8ad
Eduardo <medina.eag@gmail.com>
2020-09-20 16:17:27 -0400

    Fix errors: update to src/Special.php, update extention.json, add alias file

---
+++ Blender.i18n.alias.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Aliases for myextension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English
+ * @author <your username>
+ */
+$specialPageAliases['en'] = [
+	'Blender' => [ 'Blender', 'My Blender' ],
+];
+
+/** Spanish
+ * @author medied
+ */
+$specialPageAliases['es'] = [
+	'Blender' => [ 'Licuadora', 'Mi licuadora' ],
+];
